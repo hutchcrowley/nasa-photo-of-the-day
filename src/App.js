67368ccc0465ee3bea1e978ee7028523fa1acb8a
Hutch Crowley <tothecoda@gmail.com>
2019-10-25 14:32:37 -0500

    Update- stable build, w/ styled components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,62 @@
-import React, { useState, useEffect } from 'react'
-import '../public/favicon.ico'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 
-import axios from 'axios'
-import './App.css'
+const Button = styled.button`
+    height: 100px;
+    width: 100px;
+    border-radius: 50px;
+    color: #ffff;
+    background: black;
+  &:hover {
+    background: #0000;
+    color: black;
+}
+`
 
-function App() {
+function App (props)
+{
   const [data, setData] = useState([])
-  useEffect(() => {
+
+
+  useEffect(() =>
+  {
     axios
       .get(
         'https://api.nasa.gov/planetary/apod?api_key=zgbzTnPJh7YJt0VsZMynxjp3fx6qW15PZhlJ3aob'
       )
-      .then((res) => {
+      .then((res) =>
+      {
         setData(res.data)
         console.log(res)
       })
-      .catch((err) => {
+      .catch((err) =>
+      {
         console.log(err)
       })
   }, [])
 
   return (
-    <div className="card-wrapper">
+
+    <div id="App">
       <img src={data.url} alt="NASA apod" />
-      <h5>{data.date}</h5>
-      <h1>{data.title}</h1>
-      <p>{data.explanation}</p>
+      <Button>
+        <a href='https://wwww.nasa.com'>
+          Click me!
+            </a>
+      </Button>
+      <h2>
+        {data.title}
+      </h2>
+      <h5>
+        {data.date}
+      </h5>
+
+      <p>
+        {data.description}
+      </p>
     </div>
+
   )
 }
 
